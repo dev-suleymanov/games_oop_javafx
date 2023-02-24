2075e450e68ffcbef6ac6fd24c350635f870f143
dev-suleymanov <dev.suleymanov@gmail.com>
2023-02-24 13:14:12 +0300

    3. Каркас шахматной доски [#792 #392862]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -1,34 +1,41 @@
 package ru.job4j.chess;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.black.BishopBlack;
-import ru.job4j.chess.firuges.black.QueenBlack;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LogicTest {
 
-    @Test(expected = ImpossibleMoveException.class)
-    public void whenMoveThenImpossibleMoveException()
-            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+    @Test
+    public void whenMoveThenFigureNotFoundException() {
         Logic logic = new Logic();
-        logic.add(new BishopBlack(Cell.C1));
-        logic.move(Cell.C1, Cell.C2);
+        FigureNotFoundException exception = assertThrows(FigureNotFoundException.class, () -> {
+            logic.move(Cell.C1, Cell.H6);
+        });
+        assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
     }
 
-    @Test(expected = FigureNotFoundException.class)
-    public void whenMoveThenFigureNotFoundException()
-            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+    @Test
+    public void whenOccupiedCellException() {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C1));
-        logic.move(Cell.C3, Cell.C2);
+        logic.add(new BishopBlack(Cell.D2));
+        OccupiedCellException exception = assertThrows(OccupiedCellException.class, () -> {
+            logic.move(Cell.C1, Cell.G5);
+        });
+        assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
     }
 
-    @Test(expected = OccupiedCellException.class)
-    public void whenMoveThenOccupiedCellException()
-            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+    @Test
+    public void whenImpossibleMoveException() {
         Logic logic = new Logic();
-        logic.add(new BishopBlack(Cell.C1));
-        logic.add(new QueenBlack(Cell.D2));
-        logic.move(Cell.C1, Cell.E3);
+        logic.add(new BishopBlack(Cell.C2));
+        ImpossibleMoveException exception = assertThrows(ImpossibleMoveException.class, () -> {
+            logic.move(Cell.C2, Cell.G5);
+        });
+        assertThat(exception.getMessage()).isEqualTo("Could not way by diagonal from C2 to G5");
     }
 }
